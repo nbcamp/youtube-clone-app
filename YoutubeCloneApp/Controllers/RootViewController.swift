8f9ee347bb6ea50292a2d584fe7743755c1fd69b
jinyongp <dev.jinyongp@gmail.com>
2023-09-04 14:54:47 +0900

    init: Main.storyboard 제거

---
+++ YoutubeCloneApp/Controllers/RootViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class RootViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
