db8d06de101797a0e0f2ba6763a19b6366bfc521
jinyongp <dev.jinyongp@gmail.com>
2023-09-09 18:08:26 +0900

    feat: 프로필 화면 작성

--- YoutubeCloneApp/Controllers/ProfileViewController.swift
+++ YoutubeCloneApp/Controllers/ProfileViewController.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-final class ProfileViewController: TypedViewController<ProfileView> {}
+final class ProfileViewController: TypedViewController<ProfileView> {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        AuthService.shared.$user.subscribe(by: self, immediate: true) { subscriber, user in
+            if let user = user.new {
+                subscriber.rootView.configure(user: user)
+            }
+        }
+    }
+}
--- YoutubeCloneApp/Views/ProfileView/ProfileView.swift
+++ YoutubeCloneApp/Views/ProfileView/ProfileView.swift
@@ -1,21 +1,75 @@
 import UIKit
 
 final class ProfileView: UIView, RootView {
-    private lazy var label = {
-        let label = UILabel()
-        label.text = "Profile View"
-        label.sizeToFit()
-        return label
-    }()
-    
-    func initializeUI() {
-        addSubview(label)
-        
-        backgroundColor = .systemBackground
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+    private lazy var containerView = {
+        let containerView = UIStackView(arrangedSubviews: [
+            avatarView,
+            nameLabel,
+            dividerView,
+            emailLabel,
         ])
+        containerView.axis = .vertical
+        containerView.alignment = .center
+        containerView.spacing = 15.0
+        avatarView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            avatarView.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.4),
+            avatarView.heightAnchor.constraint(equalTo: avatarView.widthAnchor, multiplier: 0.95),
+
+            dividerView.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.3),
+            dividerView.heightAnchor.constraint(equalToConstant: 1),
+        ])
+        return containerView
+    }()
+
+    private lazy var avatarView = {
+        let avatarView = UIImageView()
+        avatarView.image = .init(systemName: "person.circle")
+        return avatarView
+    }()
+
+    private lazy var nameLabel = {
+        let nameLabel = UILabel()
+        return nameLabel
+    }()
+
+    private lazy var dividerView = {
+        let dividerView = UIView()
+        dividerView.backgroundColor = .systemGray
+        return dividerView
+    }()
+
+    private lazy var emailLabel = {
+        let emailLabel = UILabel()
+
+        return emailLabel
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { fatalError() }
+
+    func initializeUI() {
+        backgroundColor = .systemBackground
+        addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        let guide = safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 40),
+            containerView.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
+        ])
+    }
+
+    func configure(user: User) {
+        if let avatar = user.uiAvatar {
+            avatarView.image = avatar
+        }
+        nameLabel.text = user.name
+        emailLabel.text = user.email
     }
 }
