59972d9db945abeefbf1f25ff062dc3bf8559171
Park, Jinyong <dev.jinyongp@gmail.com>
2023-09-10 08:41:22 +0900

    프로필 화면 추가 및 사용자 정보 변경 기능 추가 (#6)

--- YoutubeCloneApp/Controllers/ProfileViewController.swift
+++ YoutubeCloneApp/Controllers/ProfileViewController.swift
@@ -1,3 +1,65 @@
+import PhotosUI
 import UIKit
 
-final class ProfileViewController: TypedViewController<ProfileView> {}
+struct UpdateUserAvatarEvent: EventProtocol {
+    let payload: Void = ()
+}
+
+struct UpdateUserProfileEvent: EventProtocol {
+    struct Payload {
+        let name: String
+        let email: String
+    }
+
+    let payload: Payload
+}
+
+final class ProfileViewController: TypedViewController<ProfileView> {
+    private var user: User? { AuthService.shared.user }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        rootView.user = user
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        EventBus.shared.on(UpdateUserAvatarEvent.self, by: self) { listener, _ in
+            var configuration = PHPickerConfiguration()
+            configuration.selectionLimit = 1
+            configuration.filter = .images
+            let pickerVC = PHPickerViewController(configuration: configuration)
+            pickerVC.delegate = listener
+            pickerVC.modalPresentationStyle = .popover
+            listener.present(pickerVC, animated: true)
+        }
+
+        EventBus.shared.on(UpdateUserProfileEvent.self, by: self) { listener, payload in
+            listener.user?.name = payload.name
+            listener.user?.email = payload.email
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        EventBus.shared.reset(self)
+    }
+}
+
+extension ProfileViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+
+        guard let itemProvider = results.first?.itemProvider,
+              itemProvider.canLoadObject(ofClass: UIImage.self)
+        else { return }
+
+        itemProvider.loadObject(ofClass: UIImage.self) {  [weak self] image, error in
+            guard let self, let selectedImage = image as? UIImage else { return }
+            self.user?.uiAvatar = selectedImage
+        }
+    }
+}
--- YoutubeCloneApp/Views/ProfileView/ProfileView.swift
+++ YoutubeCloneApp/Views/ProfileView/ProfileView.swift
@@ -1,21 +1,167 @@
 import UIKit
 
 final class ProfileView: UIView, RootView {
-    private lazy var label = {
-        let label = UILabel()
-        label.text = "Profile View"
-        label.sizeToFit()
-        return label
-    }()
-    
-    func initializeUI() {
-        addSubview(label)
-        
-        backgroundColor = .systemBackground
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+    weak var user: User? { didSet { observeUserChanged(user: user) } }
+
+    private var isEditMode = false
+
+    private lazy var containerView = {
+        let containerView = UIStackView(arrangedSubviews: [
+            avatarView,
+            nameTextField,
+            dividerView,
+            emailTextField,
+            editButton,
         ])
+        containerView.axis = .vertical
+        containerView.alignment = .center
+        containerView.spacing = 15.0
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            dividerView.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.3),
+            dividerView.heightAnchor.constraint(equalToConstant: 1),
+        ])
+        return containerView
+    }()
+
+    private lazy var avatarView = {
+        let avatarView = UIView()
+        avatarView.tintColor = .systemGray
+        avatarView.layer.borderWidth = 5.0
+        avatarView.layer.borderColor = UIColor.systemGray.cgColor
+
+        let width: CGFloat = 150
+        avatarView.layer.cornerRadius = width / 2
+        avatarView.layer.masksToBounds = true
+        avatarView.addSubview(avatarImageView)
+        avatarView.translatesAutoresizingMaskIntoConstraints = false
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            avatarView.widthAnchor.constraint(equalToConstant: width),
+            avatarView.heightAnchor.constraint(equalTo: avatarView.widthAnchor),
+
+            avatarImageView.topAnchor.constraint(equalTo: avatarView.topAnchor),
+            avatarImageView.bottomAnchor.constraint(equalTo: avatarView.bottomAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: avatarView.leadingAnchor),
+            avatarImageView.trailingAnchor.constraint(equalTo: avatarView.trailingAnchor),
+        ])
+        return avatarView
+    }()
+
+    private lazy var avatarImageView = {
+        let avatarImageView = UIImageView()
+        avatarImageView.image = .init(systemName: "photo")
+        avatarImageView.transform = .init(scaleX: 0.4, y: 0.4)
+        avatarImageView.contentMode = .scaleAspectFit
+        avatarImageView.tintColor = .systemGray
+        return avatarImageView
+    }()
+
+    private lazy var nameTextField = {
+        let nameTextField = UITextField()
+        nameTextField.delegate = self
+        return nameTextField
+    }()
+
+    private lazy var dividerView = {
+        let dividerView = UIView()
+        dividerView.backgroundColor = .systemGray
+        return dividerView
+    }()
+
+    private lazy var emailTextField = {
+        let emailTextField = UITextField()
+        emailTextField.delegate = self
+        return emailTextField
+    }()
+
+    private lazy var editButton = {
+        let editButton = UIButton()
+        editButton.setTitle("Edit", for: .normal)
+        editButton.setTitleColor(.label, for: .normal)
+        editButton.titleLabel?.font = .systemFont(ofSize: 14)
+        editButton.backgroundColor = .systemGray4
+        editButton.contentEdgeInsets = .init(top: 5, left: 10, bottom: 5, right: 10)
+        editButton.layer.cornerRadius = 5.0
+        return editButton
+    }()
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        dismissKeyboard()
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        dismissKeyboard()
+    }
+
+    func initializeUI() {
+        backgroundColor = .systemBackground
+        addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        let guide = safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 40),
+            containerView.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
+        ])
+
+        avatarView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(avatarViewTapped)))
+        editButton.addTarget(self, action: #selector(editButtonTapped), for: .touchUpInside)
+    }
+
+    private func observeUserChanged(user: User?) {
+        user?.$avatar.subscribe(by: self, immediate: true) { subscriber, _ in
+            guard let avatar = subscriber.user?.uiAvatar else { return }
+            DispatchQueue.main.async {
+                subscriber.avatarImageView.image = avatar
+                subscriber.avatarImageView.transform = .init(scaleX: 1.5, y: 1.5)
+            }
+        }
+        user?.$name.subscribe(by: self, immediate: true) { subscriber, changes in
+            subscriber.nameTextField.text = changes.new
+        }
+        user?.$email.subscribe(by: self, immediate: true) { subscriber, changes in
+            subscriber.emailTextField.text = changes.new
+        }
+    }
+
+    @objc private func avatarViewTapped() {
+        guard isEditMode else { return }
+        EventBus.shared.emit(UpdateUserAvatarEvent())
+    }
+
+    @objc private func editButtonTapped() {
+        isEditMode.toggle()
+        editButton.setTitle(isEditMode ? "Done" : "Edit", for: .normal)
+        dismissKeyboard()
+    }
+
+    private func dismissKeyboard() {
+        endEditing(true)
+    }
+}
+
+extension ProfileView: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return isEditMode
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text, !text.isEmpty else {
+            user?.$name.publish()
+            user?.$email.publish()
+            return
+        }
+
+        EventBus.shared.emit(
+            UpdateUserProfileEvent(
+                payload: .init(
+                    name: nameTextField.text ?? "",
+                    email: emailTextField.text ?? ""
+                )
+            )
+        )
     }
 }
